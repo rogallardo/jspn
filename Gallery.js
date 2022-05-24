413deee5c1f14c61ad40a2899abeac6117795622
rogallardo <rodrigogastongallardo@gmail.com>
2022-05-24 16:13:26 -0300

    sp7

--- Gallery.js
+++ Gallery.js
@@ -1,4 +1,4 @@
-
+////////////////////////////////USO DE FETCH PARA LA GALERIA (API DE PEXEL con contenido de bicicleta)////////////////////////////////
 let cards = ``
     const containerr = document.getElementById("imgg-container")
 function getPhotos(images) {
@@ -10,25 +10,16 @@ function getPhotos(images) {
                     </a>`;
                     containerr.innerHTML += cards;
             }) 
-    //         for (i = 0; i < images.length; i++) {
-    //         const element = images[i];
-        
-              
-    // }
- 
  }
-
-     fetch("https://api.pexels.com/v1/search?query=bicycles",{
-        headers: {
+fetch("https://api.pexels.com/v1/search?query=bicycles",{
+    headers: {
           Authorization: "563492ad6f9170000100000125d3b5db766145668c7b8ff7c3a600f0"
-        }
-      })
-         .then(resp => {
+          }
+})
+.then(resp => {
            return resp.json()
-         })
-         .then(data => {
-            getPhotos(data.photos);
-            
-
-         })  
+})
+.then(data => {
+    getPhotos(data.photos);
+})  
         
