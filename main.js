f393acfad5d6b8275c237e622d603b2f4e2f6560
rogallardo <rodrigogastongallardo@gmail.com>
2022-05-24 21:12:23 -0300

    sp-8

--- main.js
+++ main.js
@@ -14,18 +14,6 @@ function menuHide() {
     lastScroll = currentScroll;
   })
 }
-////////////////////////////////////API DE GOOGLE MAPS (se muestra en la seccion 'Nosotros')////////////////////
-function iniciarMap(){
-  var coord = {lat:-34.574868665319066, lng: -58.483606227087144}; 
-  var map = new google.maps.Map(document.getElementById('map'),{
-    zoom: 15,
-    center: coord
-  });
-  var marker = new google.maps.Marker({
-    position: coord,
-    map: map
-  });
-}
 menuHide()
-iniciarMap()
+
 
